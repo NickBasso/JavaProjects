7e64573684c0e0fd37b2cd9b6442f44f26734a05
Nick <NickBasso@users.noreply.github.com>
2020-01-28 00:50:56 +0200

    upd

--- Train/src/com/base/Main.java
+++ Train/src/com/base/Main.java
@@ -1,17 +1,37 @@
 package com.base;
 
-        import java.io.File;
-        import java.io.FileNotFoundException;
-        import java.util.Scanner;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         File file = new File("src/com/base/input");
         Scanner scan = new Scanner(file);
         System.out.println(scan.next());
         System.out.println(scan.next());
         System.out.println(scan.next());
         System.out.println(scan.nextInt());
+
+        PrintNewLine();
+        BufferedReaderInput();
+    }
+
+    public static void BufferedReaderInput() throws Exception{
+        // Java Program to illustrate reading from FileReader
+        // using BufferedReader
+
+            File file = new File("src/com/base/input");
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String st;
+            while ((st = br.readLine()) != null)
+                System.out.println(st);
+    }
+
+    public static void PrintNewLine(){
+        System.out.println("\n\n\n");
     }
 }
