7eeeea9c151c1d60f8fe3eb80b7cbbac87705cf7
Nick <NickBasso@users.noreply.github.com>
2020-02-23 14:41:25 +0200

    GitUPD

--- Train/src/base/Main.java
+++ Train/src/base/Main.java
@@ -1,11 +1,30 @@
 package base;
 
 import Algorithms.Graphs.KruskalMST;
+import Algorithms.Graphs.PrimMST_AdjacencyList_BinaryHeap;
+import Algorithms.Graphs.PrimMST_AdjacencyMatrix;
 
 public class Main{
 
     public static void main(String[] args) throws Exception {
-        KruskalMST MST = new KruskalMST();
-        MST.drive();
+        KruskalMST Kruskal = new KruskalMST();
+        Kruskal.drive();
+
+        System.out.println();
+        System.out.println();
+
+        PrimMST_AdjacencyMatrix PrimMatrix = new PrimMST_AdjacencyMatrix();
+        PrimMatrix.drive();
+
+        System.out.println();
+        System.out.println();
+
+        PrimMST_AdjacencyList_BinaryHeap PrimListBinaryHeap = new PrimMST_AdjacencyList_BinaryHeap();
+        PrimListBinaryHeap.drive();
+
+        System.out.println();
+        System.out.println();
+
+
     }
 }
